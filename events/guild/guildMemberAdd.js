a87e24028ba17c0f1e158d724d1b7f26b690442c
Heimsk <64988539+Heimsk@users.noreply.github.com>
2020-10-31 17:10:03 -0300

    Delete guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, member) => {
-  if(!member.user.bot) {
-    member.roles.add('746084449179009464')
-  } else if(member.user.bot){
-    member.roles.add('745756389741428917')
-  }
-}
