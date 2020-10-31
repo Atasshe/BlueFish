d458d4149bd8f026bae2decb0e4288fa8b3bb6c4
Heimsk <64988539+Heimsk@users.noreply.github.com>
2020-10-31 17:10:45 -0300

    Update guildMemberRemove.js

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,12 +1,12 @@
 
 module.exports = (client, member) => {
   var db = client.db
-  let b = db.ref(`BlueF/Donos/${member.user.id}/Bot`).once("value").then(async function(snap) {
+  let b = db.ref(`BotList)Donos/${member.user.id}/Bot`).once("value").then(async function(snap) {
   if(snap.val() != null) {
     let bo = member.guild.members.cache.get(snap.val().bot)
     bo.kick('Dono saiu do servidor')
-    db.ref(`BlueF/Bots/${snap.val().bot}/Infos`).set(null)
-client.channels.cache.get('745387874324840539').send(`<:mai1:746470586032849007> | O bot \`${bo.user.username}\` foi expulso automáticamente pelo motivo: \`Dono saiu do servidor\``)
+    db.ref(`BotList/Bots/${snap.val().bot}/Infos`).set(null)
+client.channels.cache.get('id do canal de logs').send(`<:mai1:746470586032849007> | O bot \`${bo.user.username}\` foi expulso automáticamente pelo motivo: \`Dono saiu do servidor\``)
 
   }
   })
